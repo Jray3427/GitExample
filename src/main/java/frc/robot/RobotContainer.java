4da5c191025b58b738e6c90fdfb93b13a62fd789
Ray <tanxi3003@gmail.com>
2025-02-18 16:01:11 -0500

    Added DC motor object

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
+  private final DCMotor motor = DCMotor.getNeo550(1);
   public RobotContainer() {
     configureBindings();
   }
